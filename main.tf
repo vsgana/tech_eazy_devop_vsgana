1293b64bd02e50e784ee082656f76233ad6df323
vagu sri gana satvik <srigana02@gmail.com>
2025-07-02 18:22:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,17 @@ terraform {
 provider "aws" {
   region=var.region
 }
+
 resource "aws_security_group" "seg_as" {
   name = "stg"
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -32,9 +35,8 @@ resource "aws_security_group" "seg_as" {
   }
 }
 
-
 resource "aws_key_pair" "TF_key" {
-  key_name   = "devops"
+  key_name   = "devops1"
   public_key = tls_private_key.rsa.public_key_openssh
 }
 
@@ -44,18 +46,58 @@ resource "tls_private_key" "rsa" {
 }
 resource "local_file" "TF_key" {
   content  = tls_private_key.rsa.private_key_openssh
-  filename = "devops"
+  filename = "devops1"
+}
+resource "aws_s3_bucket" "logs" {
+  bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "logs" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    id     = "expire-logs"
+    status = "Enabled"
+
+    expiration {
+      days = 7
+    }
+    filter {}
+  }
+}
 
 resource "aws_instance" "aws_dev" {
   ami = var.ami_id
   instance_type = var.instance_type
-  depends_on = [aws_security_group.seg_as]
+  depends_on = [aws_security_group.seg_as,
+     aws_iam_instance_profile.ec2_profile,
+     aws_s3_bucket.logs
+  ]
   key_name = aws_key_pair.TF_key.key_name
+  iam_instance_profile =aws_iam_instance_profile.ec2_profile.name
   tags = {
      Name = "web-${var.stage}"
      Stage = var.stage
   }
-  user_data = file("script.sh")
+  user_data = templatefile("${path.module}/scripts/scripts.sh", {
+  bucket_name =var.bucket_name
+})
+
   }
